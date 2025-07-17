b8330451c4b10ef42d713f902551b570d57b284d
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-07-17 12:46:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors({origin:["https://blood-bank-app-1-euqx.onrender.com","http://localhost:5173"],credentials:true}));
+app.use(cors({origin:["https://blood-bank-app-1-euqx.onrender.com"],credentials:true}));
 app.use(morgan("dev"));
 
 //routes
