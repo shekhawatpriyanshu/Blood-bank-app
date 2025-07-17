6829d81effd7d4af5cc987f0ef1be942f0da8a92
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-07-17 10:49:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors(origin:"http://localhost:8080/api/v1",credentials:true));
+app.use(cors({origin:"http://localhost:8080/api/v1",credentials:true}));
 app.use(morgan("dev"));
 
 //routes
